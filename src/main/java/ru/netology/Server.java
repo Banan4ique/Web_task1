d8c6004222def23abaaf5f5910df31116a0a7395
Denis Lipatov <nutela113@yandex.ru>
2025-05-26 23:01:52 +0300

    Fixed constructor with query params and post params, fixed problem with downloading existing files

--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -11,8 +11,9 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.apache.http.NameValuePair;
 import ru.netology.parts.Part;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 public class Server {
     private final Set<String> staticFiles = ConcurrentHashMap.newKeySet();
@@ -51,13 +52,13 @@ public class Server {
             Request request = parseRequest(in);
             if (request == null) return;
 
-            System.out.println("Пришёл запрос: " + request.method() + " " + request.path());
+            System.out.println("Пришёл запрос: " + request.getMethod() + " " + request.getPath());
             System.out.println("Параметры запроса:");
-            request.queryParams().stream().filter(Objects::nonNull).map(x -> x.getName() + "=" + x.getValue())
+            request.getQueryParams().stream().filter(Objects::nonNull).map(x -> x.getName() + "=" + x.getValue())
                     .forEach(System.out::println);
 
             System.out.println("Тело запроса:");
-            request.bodyParams().stream().filter(Objects::nonNull).map(x -> x.getName() + "=" + x.getValue())
+            request.getPostParams().stream().filter(Objects::nonNull).map(x -> x.getName() + "=" + x.getValue())
                     .forEach(System.out::println);
 
             if (request.isMultipart()) {
@@ -70,7 +71,7 @@ public class Server {
 
                         // Сохранение файла
                         try (InputStream is = part.getInputStream()) {
-                            Files.copy(is, Path.of("uploads", part.getFileName()));
+                            Files.copy(is, Path.of("uploads", part.getFileName()), REPLACE_EXISTING);
                         }
                     } else {
                         System.out.println("Поле: " + part.getName());
@@ -79,26 +80,27 @@ public class Server {
                 }
             }
 
-            String subPath = request.path().contains("?") ?
-                    request.path().substring(0, request.path().indexOf("?")) :
-                    request.path();
+            String subPath = request.getPath().contains("?") ?
+                    request.getPath().substring(0, request.getPath().indexOf("?")) :
+                    request.getPath();
+
             // Проверяем есть ли кастомный обработчик для этого пути и метода
-            if (handlers.containsKey(request.method())) {
-                if(handlers.get(request.method()).entrySet().stream()
+            if (handlers.containsKey(request.getMethod())) {
+                if(handlers.get(request.getMethod()).entrySet().stream()
                         .anyMatch(x -> x.getKey().startsWith(subPath))) {
-                    System.out.println("Найден обработчик для " + request.method() + " " + request.path());
-                    handlers.get(request.method()).get(request.path()).handle(request, out);
+                    System.out.println("Найден обработчик для " + request.getMethod() + " " + request.getPath());
+                    handlers.get(request.getMethod()).get(request.getPath()).handle(request, out);
                     return;
                 }
             }
 
             // Если нет кастомного обработчика, проверяем статические файлы
             if (staticFiles.contains(subPath)) {
-                handleStaticFile(request.path(), out);
+                handleStaticFile(request.getPath(), out);
                 return;
             }
 
-            System.out.println("Не найден обработчик для " + request.method() + " " + request.path());
+            System.out.println("Не найден обработчик для " + request.getMethod() + " " + request.getPath());
             sendNotFound(out);
         } catch (IOException e) {
             e.printStackTrace();
@@ -198,9 +200,6 @@ public class Server {
             body.append(buffer);
         }
 
-        Request request = new Request(method, path, headers, body.toString(), null, null);
-        List<NameValuePair> queryParams = request.getQueryParams();
-        List<NameValuePair> bodyParams = request.getPostParams();
-        return new Request(method, path, headers, body.toString(), queryParams, bodyParams);
+        return new Request(method, path, headers, body.toString());
     }
 }
